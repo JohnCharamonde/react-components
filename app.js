4d96467dde6f3223a2cbb75e8ac28b7ead5f9f25
JohnCharamonde <john.charamonde@gmail.com>
2019-12-30 16:20:33 -0500

    Completed everything except list item bold on hover

--- app.js
+++ app.js
@@ -1,1 +1,90 @@
-// TODO
+
+//CREATING AND RENDERING A REACT COMPONENT
+
+// var GroceryList = () => (
+//   <ul>
+//     <li>Milk</li>
+//     <li>Cookies</li>
+//   </ul>
+// );
+
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+//CREATIING AND RENDERING NESTED REACT COMPONENTS
+
+
+// var GroceryList = () => (
+//   <ul>
+//     <li><Milk/></li>
+//     <li><Cookies/></li>
+//   </ul>
+// );
+
+// var Milk = () => (
+//   <div>Milk</div>
+// );
+
+// var Cookies = () => (
+//   <div>Cookies</div>
+// );
+
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+// COMPONENT PROPERTIES AKA PROPS
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <li>{this.props.listItem}</li>
+//     );
+//   };
+// };
+
+// var GroceryList = (props) => (
+//   <ul>
+//     {props.items.map(listItem =>
+//       <GroceryListItem listItem = {listItem}/>
+//     )}
+//   </ul>
+// );
+
+// ReactDOM.render(<GroceryList items={['Cookies', 'Milk']} />, document.getElementById("app"));
+
+//LI BOLD ON MOUSE OVER
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      normal: true;
+    }
+  }
+
+  onListItemHover() {
+    this.setState({
+      this.state.normal = !this.state.normal
+    });
+  };
+
+  render() {
+  var style = {
+    textDecoration: this.state.normal ? 'none' : 'bold';
+  };
+  return (
+    <li style={style} onHover={this.onListItemHover.bind(this)}>{this.props.listItem}</li>
+  )};
+};
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(listItem =>
+      <GroceryListItem listItem = {listItem}/>
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList items={['Cookies', 'Milk']} />, document.getElementById("app"));
